bd37b4b26883d577b2ca47ae9b2f2432ef6d076f
Alexis Métaireau <alexis@notmyidea.org>
2016-11-20 19:27:21 +0100

    Generate the index page with pelican

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHORS = (
+    u'The Kinto team',
+)
+
+SITENAME = u'Kinto Storage'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'fr'
+
+DEFAULT_PAGINATION = False
+
+THEME = "theme"
+INDEX_SAVE_AS = 'news.html'
+
+MENUITEMS = (
+    (u'About Kinto', '/index.html'),
+    (u'News', '/news.html'),
+)
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+STATIC_PATHS = ['images', 'documents', 'extra/CNAME', ]
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
+
+RELATIVE_URLS = True
