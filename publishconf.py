3a7b3ce70d9111448146a17071438217f6bd02dd
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-01-06 11:59:05 +0100

    Switching to https.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *  # NoQA
 
-SITEURL = 'http://www.kinto-storage.org'
+SITEURL = 'https://www.kinto-storage.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
